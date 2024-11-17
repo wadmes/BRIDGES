fa08d085739af0c21544b501150a5a4058bfcd56
wadmes <werry715@gmail.com>
2024-11-16 22:09:23 -0500

    update scripts

--- VLSI_util/create_data.sh
+++ VLSI_util/create_data.sh
@@ -1,6 +1,6 @@
 # python data_textonly.py --netlist_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/netlist_data/netlist.json --rtl_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/rtl_data/rtl.json --name RTLCoder26532-textonly
-python data.py
-python data.py --netlist_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/netlist_data/netlist.json --rtl_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/rtl_data/rtl.json --name RTLCoder26532
+# python data.py
+# python data.py --netlist_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/netlist_data/netlist.json --rtl_path /home/weili3/VLSI-LLM/data_collection/RTLCoder26532/rtl_data/rtl.json --name RTLCoder26532
 cp ./RTLCoder26532.pt /scratch/weili3/
 cp ./MGVerilog11144.pt /scratch/weili3/
 python split_data.py --graph_path /scratch/weili3/RTLCoder26532.pt
--- submit_stage1.sh
+++ submit_stage1.sh
@@ -1,7 +1,19 @@
 
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.05 --training_data_used 0.05 &
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.01 --training_data_used 0.01 &
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.1 --training_data_used 0.1 &
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.2 --training_data_used 0.2 &
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.5 --training_data_used 0.5 &
-srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.8 --training_data_used 0.8 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.05 --training_data_used 0.05 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.01 --training_data_used 0.01 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.1 --training_data_used 0.1 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.2 --training_data_used 0.2 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.5 --training_data_used 0.5 &
+# srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty -w node-gpu02 --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.8 --training_data_used 0.8 &
+
+
+
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty  --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.1_dim_128 --training_data_used 0.1 --gin_hidden_dim 128 &
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty  --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.5_dim_128 --training_data_used 0.5 --gin_hidden_dim 128&
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn  --filename dim_128 --gin_hidden_dim 128 &
+
+
+
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.1_dim_256 --training_data_used 0.1 --gin_hidden_dim 256 &
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn --filename train_0.5_dim_256 --training_data_used 0.5 --gin_hidden_dim 256&
+srun --time 3-23 --mem=400G --cpus-per-gpu=4 --pty --gres gpu:H100:1 --reservation=ConfNov19 python stage1.py --gtm --lm --tune_gnn  --filename dim_256 --gin_hidden_dim 256 &
